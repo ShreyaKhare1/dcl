c3a0f1ea3e4c9d5b04b89ad4720973adac02ed4e
ShreyaKhare1 <120880000+ShreyaKhare1@users.noreply.github.com>
2024-07-02 22:51:37 +0530

    Update rigsoft.py

--- rigsoft.py
+++ rigsoft.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 @app.route('/')
 def main():
-  return render_template('main.html')
+  return render_template('index.html')
 @app.route('/basic computation')
 def basic_computation():
   return render_template('basic computation.html')
